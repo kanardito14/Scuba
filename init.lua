8a3e233c44c2207bb34311a116924a133ec23305
Jeff <kanardito14@free.fr>
2016-01-24 16:11:01 +0100

    Air capacity is a tank property, not a player one.

--- init.lua
+++ init.lua
@@ -8,17 +8,35 @@ local function scuba()
    local players = minetest.get_connected_players()
    for i, player in ipairs(players) do
 
-      --no tank in main inventory means no scuba patch
-      local playerinv = minetest.get_inventory({type="player", name=player:get_player_name()})
-      if playerinv:contains_item("main", "scuba:tankfull") then               
+      local pl_name   = player:get_player_name()
+      local playerinv = minetest.get_inventory({type="player", name=pl_name})
 
-	 local pl_name = player:get_player_name()
-	 local pl_num  = -1
-	 print("Scuba --> " .. pl_name)
+      -- lookup for full tank
+      local inventory = playerinv:get_list("main")
+      local meta      = nil
+      local tank      = nil
+      local tankidx   = 0
 
+      for i = 1, 8 do
+	 if inventory[i]:get_name() == "scuba:tankfull" then
+	    print ("Scuba --> Found tank on ", pl_name, "slot", i)
+	    meta = minetest.deserialize(inventory[i]:get_metadata())
+	    if meta ~= nil and meta.air ~= nil  and meta.air > 0 then
+	       tank    = inventory[i]
+	       tankidx = i
+	       print ("Scuba --> metadata.air", meta.air)
+	       break
+--	       inventory[i]:set_metadata(minetest.serialize(meta))
+--	       playerinv:set_stack("main", i, inventory[i])	       
+	    end
+	 end
+      end
+
+      if tank ~= nil then
+	 
 	 local pl_air = air[pl_name]
 	 if pl_air == nil then break end
-
+	 
 	 if player:get_breath() < 10 then
 	    if pl_air.zid == -1 then
 	       pl_air.zid2 = player:hud_add( {
@@ -37,17 +55,19 @@ local function scuba()
 		     offset = {x=25, y=-110},
 		     name = "scuba air gauge",
 		     text = "psi.png",
-		     number = pl_air.a,
+		     number = meta.air,
 		     alignment = {x=1,y=-1},
 		     scale = {x=1, y=1},
 	       } )
 	    else -- if zid
-	       player:hud_change(pl_air.zid, "number", pl_air.a)
+	       player:hud_change(pl_air.zid, "number", meta.air)
 	    end -- if zid
-
-	    pl_air.a = pl_air.a - 1 
-	    if pl_air.a > 0 then player:set_breath(9) end
-
+	    
+	    if meta.air > 0 then player:set_breath(9) end
+	    meta.air = meta.air - 1 
+	    tank:set_metadata(minetest.serialize(meta))
+	    playerinv:set_stack("main", tankidx, tank)	       
+	    
 	 else -- player:get_breath()
 	    if pl_air.zid ~= -1 then
 	       player:hud_remove(pl_air.zid)
@@ -55,15 +75,17 @@ local function scuba()
 	       pl_air.zid = -1
 	    end
 	 end -- player:get_breath()
-      end -- tank in main inventory patch
+      end -- if tank ~= nil
    end --for players
 end
 
 minetest.after(3, function()
 		  minetest.register_globalstep(function(dtime)
 			timer = timer + dtime
+			-- print("Scuba timer (timer, dtime)", timer, dtime)
 			if timer < 3 then return end
 			timer = 0
+			-- print("Scuba timer reset")
 			scuba()
 		  end) --function
 end) --function
@@ -248,14 +270,14 @@ minetest.register_abm({
       chance = 1,
       action = function(pos, node, active_object_count, active_object_count_wider)
 	 local meta = minetest.get_meta(pos)
-	 for i, name in ipairs({
-	       "src_totaltime",
-	       "src_time"
-	 }) do
-	    if meta:get_string(name) == "" then
-	       meta:set_float(name, 0.0)
-	    end
-	 end
+	 -- for i, name in ipairs({"src_totaltime", "src_time"}) do
+	 --    if meta:get_string(name) == "" then
+	 --       meta:set_float(name, 0.0)
+	 --    end
+	 -- end
+
+	 meta:set_float("src_totaltime", 30.0)
+	 
 	 local inv = meta:get_inventory()
 	 local srclist = inv:get_list("src")
 	 local cooked = nil
@@ -264,29 +286,34 @@ minetest.register_abm({
 	    cooked, aftercooked = minetest.get_craft_result({method = "cooking", width = 1,
 							     items = srclist})
 	 end
-	 meta:set_float("src_time", meta:get_float("src_time") + 1)
-	 if cooked and cooked.item and meta:get_float("src_time") >= cooked.time then
-	    -- check if there's room for output in "dst" list
-	    if inv:room_for_item("dst",cooked.item) then
-	       -- Put result in "dst" list
-	       inv:add_item("dst", cooked.item)
-	       -- take stuff from "src" list
-	       inv:set_stack("src", 1, aftercooked.items[1])
-	    else
-	       print("Could not insert '"..cooked.item:to_string().."'")
+	 if cooked and cooked.item and not cooked.item:is_empty() then
+	    meta:set_string("infotext","Scuba Airfill Station in function")
+	    meta:set_float("src_time", meta:get_float("src_time") + 1)
+	    print ("Scuba --> cooked.time, src_time", cooked.time, meta:get_float("src_time"))
+	    if meta:get_float("src_time") >= 10 * cooked.time then
+	       -- check if there's room for output in "dst" list
+	       if inv:room_for_item("dst",cooked.item) then
+		  -- Set meta information (tank capacity)
+		  local meta2 = {air = 100}
+		  cooked.item:set_metadata(minetest.serialize(meta2))
+		  print ("Scuba --> tank filled",
+			 cooked.item:get_name(),
+			 tostring(minetest.serialize(meta2)))
+		  -- Put result in "dst" list
+		  inv:add_item("dst", cooked.item)
+                  -- take stuff from "src" list
+		  inv:set_stack("src", 1, aftercooked.items[1])
+	       else
+		  print("Could not insert '"..cooked.item:to_string().."'")
+	       end
+	       meta:set_float("src_time", 0)
 	    end
-	    meta:set_string("src_time", 0)
 	 end
-	 local cooked = nil
-	 local srclist = inv:get_list("src")
-	 if srclist then
-	    cooked = minetest.get_craft_result({method = "cooking", width = 1, items = srclist})
-	 end
-	 if cooked.item:is_empty() then
+
+	 if cooked and cooked.item and cooked.item:is_empty() then
 	    meta:set_string("infotext","Scuba Airfill Station is empty")
 	    hacky_swap_node(pos,"scuba:airfill")
 	    meta:set_string("formspec", scuba_airfill_inactive_formspec)
-	    return
 	 end
       end,
 })
